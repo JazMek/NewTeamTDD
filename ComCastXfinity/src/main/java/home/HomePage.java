f52817461fb06036abaf2efb7d1717ae562ed51b
kmekdoud <karimmekdoud@gmail.com>
2020-09-15 00:26:02 -0400

    first commit

---
+++ ComCastXfinity/src/main/java/home/HomePage.java
@@ -0,0 +1,11 @@
+package home;
+
+import common.WebAPI;
+
+public class HomePage extends WebAPI {
+
+
+    public void homePage_get_Title() {
+        getTitle("Access My Account | Email | Online News | My Xfinity®");
+    }
+}
---
+++ ComCastXfinity/src/test/java/testhome/TestHomePage.java
@@ -0,0 +1,23 @@
+package testhome;
+
+import common.WebAPI;
+import home.HomePage;
+import home.XfinityWebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+public class TestHomePage extends WebAPI {
+        public static HomePage homePage;
+        public static XfinityWebElement xfinityWebElement;
+        public static void init(){
+        homePage = PageFactory.initElements(driver,HomePage.class);
+        xfinityWebElement = PageFactory.initElements(driver,XfinityWebElement.class);
+    }
+    @Test
+    public static void homePage_get_Title_Test(){
+        init();
+        homePage.homePage_get_Title();
+
+    }
+
+}
